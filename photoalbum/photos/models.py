8256d0c4f92ce9d0dd97d4b906f2a71b0c6dba4f
Nicholas Wurzer <nwurzer@Nicks-MacBook-Pro.local>
2024-08-09 16:12:36 -0700

    Add uploader and image field to Photo Model

--- photoalbum/photos/models.py
+++ photoalbum/photos/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Photo(models.Model):
     latitude = models.DecimalField()
     longitude = models.DecimalField()
+    uploader = models.CharField()
+    image = models.ImageField()
