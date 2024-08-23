62d8b5a88130ee4934f5aee160183f7083193266
Nicholas Wurzer <nwurzer@Nicks-MacBook-Pro.local>
2024-08-23 16:01:17 -0700

    Render Photos in Development Mode, Update Gitignore

--- photoalbum/photos/models.py
+++ photoalbum/photos/models.py
@@ -4,4 +4,4 @@ class Photo(models.Model):
     latitude = models.DecimalField(decimal_places=6, max_digits=9)
     longitude = models.DecimalField(decimal_places=6, max_digits=9)
     uploader = models.CharField(max_length=30)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="images")
