d4798e46a271247349aea04f0e54ddf167b26c32
Nicholas Wurzer <nwurzer@Nicks-MacBook-Pro.local>
2024-08-23 14:24:16 -0700

    Add Photo Model to Admin Site, Display Photos on Index Page

--- photoalbum/photos/views.py
+++ photoalbum/photos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.template import loader
+from photos.models import Photo
 
 def index(request):
-    context = {}
+    context = {
+        "photos" : Photo.objects.all()
+        }
     return render(request, "photos/index.html", context)
